46564e4d4a78943c051d0466a4ac06fb73d89d2b
ryan2214 <ryan2214@users.noreply.github.com>
2020-11-03 06:47:13 +0800

    Update script.js
    modification to on air

--- public/script.js
+++ public/script.js
@@ -1,9 +1,6 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myPeer = new Peer(undefined, {
-    host: '/',
-    port: '5001'
-})
+const myPeer = new Peer(undefined, undefined)
 const myVideo = document.createElement('video')
 myVideo.muted = true
 const peers = {}
